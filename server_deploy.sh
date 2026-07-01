829e5cfce0b3a09258f572255870b70c5e4e4a39
Leonardo Carvalho <leo.zcarvalho@hotmail.com>
2026-07-01 20:07:52 -0300

    padrao container

--- server_deploy.sh
+++ server_deploy.sh
@@ -5,9 +5,7 @@ set -e
 # Configurações
 # ===========================
 APP_DIR="$HOME/sals"
-BACK_DIR="$APP_DIR/api"
 BACK_CONTAINER_NAME="api"
-BACK_PORT=8000
 
 # ===========================
 # Passo 1: Atualiza código do Git
@@ -19,31 +17,13 @@ git clean -fd
 git pull origin main
 
 # ===========================
-# Passo 3: Atualiza backend
+# Passo 2: Atualiza backend
 # ===========================
 echo "Atualizando backend..."
-cd $BACK_DIR
-
-# Para Docker:
-# Para parar container antigo, se existir
-if [ "$(docker ps -q -f name=$BACK_CONTAINER_NAME)" ]; then
-    echo "Parando container antigo..."
-    docker stop $BACK_CONTAINER_NAME
-    docker rm $BACK_CONTAINER_NAME
-fi
-
-# Build e run do container
-echo "Subindo container backend..."
-docker run -d \
-    --name $BACK_CONTAINER_NAME \
-    -p $BACK_PORT:8000 \
-    -v $BACK_DIR:/api \
-    -w /api \
-    python:3.11 \
-    bash -c "pip install --no-cache-dir -r requirements.txt && pip install --no-cache-dir python-multipart && uvicorn main:app --host 0.0.0.0 --port 8000"
+docker compose up -d --build $BACK_CONTAINER_NAME
 
 # ===========================
-# Passo 4: Recarrega Nginx
+# Passo 3: Recarrega Nginx
 # ===========================
 echo "Recarregando Nginx..."
 sudo nginx -t
